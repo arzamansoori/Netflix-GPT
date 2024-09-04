2df6d039b834ddf47ed4871d3206691196196288
arzatf <arzamansoori786@gmail.com>
2024-09-04 19:35:40 +0530

    Implemented Authentication in Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
-import {validateFormFields} from "../utils/validateForm";
+import { validateFormFields } from "../utils/validateForm";
 import { bg_url } from "../utils/images";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
@@ -13,13 +15,61 @@ const Login = () => {
 
   const toggleBtnSignUpForm = () => {
     return setIsSignInForm(!isSignInForm);
-  }
+  };
 
-  const validateFields = () =>{
-    const errMessage = validateFormFields(email.current.value, password.current.value);
+  const validateFields = () => {
+    //check if not valid
+    const errMessage = validateFormFields(
+      email.current.value,
+      password.current.value
+    );
     setErrorMessage(errMessage);
-  }
+    if (errMessage) return;
 
+    //check if valid
+    if (!isSignInForm) {
+      //Sign Up Form
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //Sign In Form
+      //this is from Firebase docs
+      console.log("Attempting sign in");
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log("User", user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (errorCode === "auth/invalid-credential") {
+            setErrorMessage("Password or Email is incorrect.");
+          }
+          else {
+            setErrorMessage(errorMessage);
+          }
+          console.log(errorCode + errorMessage)
+        });
+        
+    }
+  };
 
   return (
     <div className=" ">
@@ -29,17 +79,24 @@ const Login = () => {
           <img src={bg_url} alt="backgoundImage" />
         </div>
         <div className="relative grid h-screen place-items-center">
-          <form onSubmit={(e)=>{e.preventDefault()}} className="flex flex-col items-start justify-center bg-opacity-50 bg-black py-[48px] px-[68px]">
+          <div
+            // onSubmit={(e) => {
+            //   e.preventDefault();
+            // }}
+            className="flex flex-col items-start justify-center bg-opacity-50 bg-black py-[48px] px-[68px]"
+          >
             <div>
               <h1 className="font-bold text-white text-4xl my-6">
                 {isSignInForm ? "Sign in" : "Sign up"}
-                </h1>
+              </h1>
             </div>
-            {(!isSignInForm) && <input
-              type="text"
-              placeholder="Full Name"
-              className="p-3 px-5 my-4 text-lg text-white border-inherit border rounded-md bg-black bg-opacity-70"
-            />}
+            {!isSignInForm && (
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="p-3 px-5 my-4 text-lg text-white border-inherit border rounded-md bg-black bg-opacity-70"
+              />
+            )}
             <input
               ref={email}
               type="text"
@@ -47,23 +104,29 @@ const Login = () => {
               className="p-3 px-5 my-4 text-lg text-white border-inherit border rounded-md bg-black bg-opacity-70"
             />
             <input
-             ref={password}
+              ref={password}
               type="password"
               placeholder="Password"
               className="p-3 px-5 my-2 text-lg text-white border-inherit border rounded-md bg-black bg-opacity-70"
-              
             />
             <p className="text-red-600 font-semibold">{errorMessage}</p>
-            <button className="p-2 my-2 bg-red-600 rounded-md w-[100%] text-white font-semibold" onClick={validateFields}>
-            {isSignInForm ? "Sign In" : "Sign Up"}
+            <button
+            //  type="submit"
+              className="p-2 my-2 bg-red-600 rounded-md w-[100%] text-white font-semibold"
+              onClick={validateFields}
+            >
+              {isSignInForm ? "Sign In" : "Sign Up"}
             </button>
             <p className="text-slate-300 my-4" onClick={toggleBtnSignUpForm}>
-            {isSignInForm ? "New to Netflix?" : "Already Registered?"} 
-              <span className="font-semibold text-white cursor-pointer ml-1 hover:border-b" onClick={toggleBtnSignUpForm}>
-                  {isSignInForm ? "Sign up now." : "Sign in now."}
+              {isSignInForm ? "New to Netflix?" : "Already Registered?"}
+              <span
+                className="font-semibold text-white cursor-pointer ml-1 hover:border-b"
+                onClick={toggleBtnSignUpForm}
+              >
+                {isSignInForm ? "Sign up now." : "Sign in now."}
               </span>
             </p>
-          </form>
+          </div>
         </div>
       </div>
     </div>
