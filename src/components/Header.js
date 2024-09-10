432ab9532ffc63732d681363d58915eda467efab
arzatf <arzamansoori786@gmail.com>
2024-09-10 12:55:06 +0530

    Hygiene practice: unsubscribed to the onAuthStateChange callback

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   const user = useSelector(store => store.user);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -25,6 +25,8 @@ const Header = () => {
         navigate("/");
       }
     });
+    //unsubscribe when the component unmounts
+    return () => unsubscribe();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
