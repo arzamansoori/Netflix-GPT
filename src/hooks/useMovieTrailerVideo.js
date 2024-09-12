5ba1576898567d7a981a705e141dc2c94aed6812
arzatf <arzamansoori786@gmail.com>
2024-09-12 16:51:29 +0530

    created custom hook to fetch videos from TMDB API & added iFrame link from youTube

---
+++ src/hooks/useMovieTrailerVideo.js
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../redux/moviesSlice";
+import { useEffect } from "react";
+
+
+const useMovieTrailerVideo = (movieId) =>{
+
+    const dispatch = useDispatch();
+
+    //fetching the trailer and updating the store with trailer video data
+    const getMovieTrailer = async () => {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US",
+        API_OPTIONS
+      );
+      const json = await data.json();
+      console.log("JSON", json);
+  
+      const filterTrailer = json.results.filter(
+        (video) => video.type === "Trailer"
+      );
+      console.log("Filter Trailer", filterTrailer);
+  
+      //there are two types of trailer that are coming
+      //what if there's no type=tailer in api
+      //handling both cases
+      const trailer = filterTrailer.length ? filterTrailer[0] : json.results[0];
+      dispatch(addTrailerVideo(trailer));
+      console.log("Trailer", trailer);
+    };
+  
+    useEffect(() => {
+      getMovieTrailer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+};
+
+export default useMovieTrailerVideo;
