04566d40cadf1c82f47a6823c26bccde6b0dafc1
arzatf <arzamansoori786@gmail.com>
2024-09-12 16:46:26 +0530

    designing browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 //import { logo_url } from "../utils/images";
 
 const Browse = () => {
-
   //using custom hook to add the now playing movies into redux store
   useNowPlayingMovies();
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { logo_url } from "../utils/images";
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { addUser, removeUser } from '../redux/userSlice';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../redux/userSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -17,7 +17,14 @@ const Header = () => {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
         const { uid, email, displayName, photoURL } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
         navigate("/browse");
       } else {
         // User is signed out
@@ -27,28 +34,37 @@ const Header = () => {
     });
     //unsubscribe when the component unmounts
     return () => unsubscribe();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const toggleSignOut = () => {
-    signOut(auth).then(() => {
-      // navigate("/");
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-    });
-  }
-
+    signOut(auth)
+      .then(() => {
+        // navigate("/");
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
 
   return (
-    <div className='absolute mx-9 my-3 z-10 flex justify-between w-[-webkit-fill-available]'>
-      <img src={logo_url} alt='Netflix logo' className='w-56'></img>
-      {user && (<div className='flex items-center mb-5'>
-        <img src={user.photoURL} alt="User_img" className='w-7 h-7 mr-1'></img>
-      <button className='font-bold text-black' onClick={toggleSignOut}>(Sign Out)</button>
-      </div>)}
+    <div className="absolute mx-9 my-3 z-10 flex justify-between w-[-webkit-fill-available]">
+      <img src={logo_url} alt="Netflix logo" className="w-56"></img>
+      {user && (
+        <div className="flex items-center mb-5">
+          <img
+            src={user.photoURL}
+            alt="User_img"
+            className="w-7 h-7 mr-1"
+          ></img>
+          <button className="font-bold text-black" onClick={toggleSignOut}>
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
