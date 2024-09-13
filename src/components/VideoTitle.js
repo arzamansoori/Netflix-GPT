8b093499b917d8c4779fcbc91e3474434c56dbc1
arzatf <arzamansoori786@gmail.com>
2024-09-13 13:51:41 +0530

    adding css in VideoTitle

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@
 
 const VideoTitle = ({title, description}) => {
     return (
-        <div className="pt-32 px-14">
-            <h1 className="font-bold text-2xl">{title}</h1>
-            <p className="w-[40%] py-6 text-lg">{description}</p>
+        <div className="absolute w-screen aspect-video bg-gradient-to-r from-black pt-[20%] px-20">
+            <h1 className="font-bold text-2xl text-white">{title}</h1>
+            <p className="w-[40%] py-6 text-lg text-white">{description}</p>
             <div>
-                <button className="bg-black text-white px-7 py-2 rounded-lg">▷ Play</button>
-                <button className="mx-2 bg-black bg-opacity-30 text-white px-7 py-2 rounded-lg">ⓘ More Info</button>
+                <button className="bg-white text-black px-7 py-2 rounded-lg hover:bg-opacity-70 font-semibold">▷ Play</button>
+                <button className="mx-2 bg-white bg-opacity-30 text-white px-3 py-2 rounded-lg hover:bg-opacity-20">ⓘ More Info</button>
             </div>
         </div>
     )
