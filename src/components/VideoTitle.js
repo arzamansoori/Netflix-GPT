346021bef90464b6c9546b74f7c36e2c1c0af302
arzatf <arzamansoori786@gmail.com>
2024-09-17 23:13:18 +0530

    adding cdn_url in constants for movies card

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -4,7 +4,7 @@ const VideoTitle = ({title, description}) => {
     return (
         <div className="absolute w-screen aspect-video bg-gradient-to-r from-black pt-[20%] px-20">
             <h1 className="font-bold text-2xl text-white">{title}</h1>
-            <p className="w-[40%] py-6 text-lg text-white">{description}</p>
+            <p className="w-[40%] py-5 text-lg text-white">{description}</p>
             <div>
                 <button className="bg-white text-black px-7 py-2 rounded-lg hover:bg-opacity-70 font-semibold">▷ Play</button>
                 <button className="mx-2 bg-white bg-opacity-30 text-white px-3 py-2 rounded-lg hover:bg-opacity-20">ⓘ More Info</button>
