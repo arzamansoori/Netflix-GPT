f6e16f62ac73a483ecf215b0e32bdfa006b28d38
arzatf <arzamansoori786@gmail.com>
2024-09-18 13:48:58 +0530

    added usePopularMovies hook, added action in redux store used it for Popluar movies component

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,11 +3,13 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
 //import { logo_url } from "../utils/images";
 
 const Browse = () => {
   //using custom hook to add the now playing movies into redux store
   useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div>
---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addPopularMovies } from "../redux/moviesSlice";
+import { useEffect } from "react";
+
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+
+  const getPopularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log("API data", json.results);
+    //this will add movies in the redux store
+    dispatch(addPopularMovies(json.results));
+  };
+
+  useEffect(() => {
+    getPopularMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export default usePopularMovies;
