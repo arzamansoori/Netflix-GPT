7077d562cbaf693880e7dc071cbd6a3a3aef1d13
arzatf <arzamansoori786@gmail.com>
2024-09-26 16:18:33 +0530

    added gpt search result in the redux store

--- src/components/MoviesCard.js
+++ src/components/MoviesCard.js
@@ -2,6 +2,7 @@ import { CDN_URL } from "../utils/images";
 
 
 const MoviesCard = ({poster_path}) => {
+    if(!poster_path) return null;
     return (
         <div className="w-48 pr-7 hover:scale-105 ease-in-out duration-300 cursor-pointer">
             <img alt="poster_img" src={CDN_URL+poster_path}></img>
--- src/redux/gptSlice.js
+++ src/redux/gptSlice.js
@@ -3,15 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 const gptSlice = createSlice({
     name : "gpt",
     initialState : {
-        showGPTSearch : false
+        showGPTSearch : false,
+        movieNames : null,
+        movieResults : null,
     },
     reducers : {
         toggleGPTSearchView : (state) => {
             state.showGPTSearch = !state.showGPTSearch;
+        },
+
+        addGPTMoviesResults : (state, action) => {
+            const { movieNames, movieResults } = action.payload;
+            state.movieNames = movieNames;
+            state.movieResults = movieResults;
+            
+        },
+
+        removeGPTMovies : (state) => {
+            state.movieNames = null;
+            state.movieResults = null;
         }
     }
 });
 
-export const {toggleGPTSearchView} = gptSlice.actions;
+export const {toggleGPTSearchView, addGPTMoviesResults, removeGPTMovies} = gptSlice.actions;
 
 export default gptSlice.reducer;
